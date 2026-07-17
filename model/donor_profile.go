134845ed5a1f64c335fdbace2562b18bc9ed18e6
azmiagr <azmiagr@gmail.com>
2026-07-17 16:40:29 +0700

    feat: add undistributed donation amount metric to donor profile response and data model

--- model/donor_profile.go
+++ model/donor_profile.go
@@ -7,27 +7,30 @@ import (
 )
 
 type DonorProfileMetricsRow struct {
-	TotalDonatedAmount float64 `json:"total_donated_amount"`
-	SupportedPostCount int64   `json:"supported_post_count"`
-	ActivePoints       int64   `json:"active_points"`
+	TotalDonatedAmount          float64 `json:"total_donated_amount"`
+	UndistributedDonationAmount float64 `json:"undistributed_donation_amount"`
+	SupportedPostCount          int64   `json:"supported_post_count"`
+	ActivePoints                int64   `json:"active_points"`
 }
 
 type DonorProfileResponse struct {
-	UserID                 uuid.UUID `json:"user_id"`
-	Name                   string    `json:"name"`
-	Initials               string    `json:"initials"`
-	Email                  string    `json:"email"`
-	PhoneNumber            string    `json:"phone_number"`
-	Role                   string    `json:"role"`
-	DisplayRole            string    `json:"display_role"`
-	KYCStatus              string    `json:"kyc_status"`
-	IsVerified             bool      `json:"is_verified"`
-	VerificationText       string    `json:"verification_text"`
-	MemberSince            time.Time `json:"member_since"`
-	MemberSinceText        string    `json:"member_since_text"`
-	Level                  string    `json:"level"`
-	TotalDonatedAmount     float64   `json:"total_donated_amount"`
-	TotalDonatedAmountText string    `json:"total_donated_amount_text"`
-	SupportedPostCount     int64     `json:"supported_post_count"`
-	ActivePoints           int64     `json:"active_points"`
+	UserID                          uuid.UUID `json:"user_id"`
+	Name                            string    `json:"name"`
+	Initials                        string    `json:"initials"`
+	Email                           string    `json:"email"`
+	PhoneNumber                     string    `json:"phone_number"`
+	Role                            string    `json:"role"`
+	DisplayRole                     string    `json:"display_role"`
+	KYCStatus                       string    `json:"kyc_status"`
+	IsVerified                      bool      `json:"is_verified"`
+	VerificationText                string    `json:"verification_text"`
+	MemberSince                     time.Time `json:"member_since"`
+	MemberSinceText                 string    `json:"member_since_text"`
+	Level                           string    `json:"level"`
+	TotalDonatedAmount              float64   `json:"total_donated_amount"`
+	TotalDonatedAmountText          string    `json:"total_donated_amount_text"`
+	UndistributedDonationAmount     float64   `json:"undistributed_donation_amount"`
+	UndistributedDonationAmountText string    `json:"undistributed_donation_amount_text"`
+	SupportedPostCount              int64     `json:"supported_post_count"`
+	ActivePoints                    int64     `json:"active_points"`
 }
